f97782dc526980ffd04b5d9e1915c8db0e45b358
advait-l <lonkaradvait@gmail.com>
2019-11-16 16:11:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Toxic-Comment-Classification
+
+A multi-label classification problem to promote good online conversations
+
+## Problem
+The problem originates from the various online forums, where people participate and make commments. The hosting organizations are constantly on the lookout for abusive, insulting or even hate-based comments to ensure that the conversations on their forums stay civil.
+
+The task is to build a model which could predict whether a comment is clean, provided various categories into which the comments are divded into : toxic,severe toxic, obscene, threat, insult, and identity hate. 
+
+## Installation Requirements
+- scikit-learn
+- scipy
+- numpy
+- pandas
+- stop_words
+- matplotlib
+- nltk
+
---
+++ visualization.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def labelBarGraph(label):
+    fig_size = plt.rcParams["figure.figsize"]
+    fig_size[0] = 10
+    fig_size[1] = 8
+    plt.rcParams["figure.figsize"] = fig_size
+    label.sum(axis=0).plot.bar()
+    plt.savefig("Visualizations/Label_Bar_Graph.png")
+    plt.show()
+
+def lengthHistogram(comment):
+    x = [len(comment[i]) for i in range(comment.shape[0])]
+    print('average length of comment: {:.3f}'.format(sum(x)/len(x)) )
+    bins = [1,200,400,600,800,1000,1200]
+    plt.hist(x, bins=bins)
+    plt.xlabel('Length of comments')
+    plt.ylabel('Number of comments')       
+    plt.axis([0, 1200, 0, 90000])
+    plt.grid(True)
+    plt.savefig("Visualizations/Length_Of_Comments_Histogram.png", bbox_inches='tight')
+    plt.show()
+
+def classifiedCommentsLengthHistogram(comment,label):
+    label = label.values
+    y = np.zeros(label.shape)
+    for i in range(comment.shape[0]):
+        l = len(comment[i])
+        if label[i][0] :
+            y[i][0] = l
+        if label[i][1] :
+            y[i][1] = l
+        if label[i][2] :
+            y[i][2] = l
+        if label[i][3] :
+            y[i][3] = l
+        if label[i][4] :
+            y[i][4] = l
+        if label[i][5] :
+            y[i][5] = l
+
+    labelsplt = ['toxic','severe_toxic','obscene','threat','insult','identity_hate']
+    color = ['red','green','blue','yellow','orange','chartreuse']
+    bins = [1,200,400,600,800,1000,1200]
+    plt.hist(y,bins = bins, label = labelsplt,color = color)
+    plt.axis([0, 1200, 0, 8000])
+    plt.xlabel('Length of comments')
+    plt.ylabel('Number of comments')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("Visualizations/Classified_Comments_Histogram.png")
+    plt.show()
+
+if __name__ == "__main__":
+    data = pd.read_csv("Data/train.csv")
+    comment = data["comment_text"]
+    label = data[['toxic', 'severe_toxic' , 'obscene' , 'threat' , 'insult' , 'identity_hate']]
+
+    labelBarGraph(label)
+    lengthHistogram(comment)
+    classifiedCommentsLengthHistogram(comment,label)
